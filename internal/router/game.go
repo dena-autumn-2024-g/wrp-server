02ed2260080521b71f0f9312c790240b7eccc1c0
mazrean <shunsuke.w04@gmail.com>
2024-10-27 10:21:19 +0900

    button push

--- internal/router/game.go
+++ internal/router/game.go
@@ -71,3 +71,61 @@ func (g *Game) Move(ctx context.Context, req *connect.Request[protogen.MoveReque
 		Msg: &protogen.MoveResponse{},
 	}, nil
 }
+
+func (g *Game) PushButton(tcx context.Context, req *connect.Request[protogen.PushButtonRequest]) (*connect.Response[protogen.PushButtonResponse], error) {
+	streamChan, err := func() (chan<- *protogen.StartGameStreamResponse, error) {
+		gameMapLocker.RLock()
+		defer gameMapLocker.RUnlock()
+
+		if _, ok := gameMap[req.Msg.RoomId]; !ok {
+			return nil, fmt.Errorf("room not found")
+		}
+
+		return gameMap[req.Msg.RoomId], nil
+	}()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get stream: %w", err)
+	}
+
+	streamChan <- &protogen.StartGameStreamResponse{
+		Event: &protogen.StartGameStreamResponse_PushButtonPressed{
+			PushButtonPressed: &protogen.PushButtonRequest{
+				UserId: req.Msg.UserId,
+				RoomId: req.Msg.RoomId,
+			},
+		},
+	}
+
+	return &connect.Response[protogen.PushButtonResponse]{
+		Msg: &protogen.PushButtonResponse{},
+	}, nil
+}
+
+func (g *Game) ReleaseButton(ctx context.Context, req *connect.Request[protogen.ReleaseButtonRequest]) (*connect.Response[protogen.ReleaseButtonResponse], error) {
+	streamChan, err := func() (chan<- *protogen.StartGameStreamResponse, error) {
+		gameMapLocker.RLock()
+		defer gameMapLocker.RUnlock()
+
+		if _, ok := gameMap[req.Msg.RoomId]; !ok {
+			return nil, fmt.Errorf("room not found")
+		}
+
+		return gameMap[req.Msg.RoomId], nil
+	}()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get stream: %w", err)
+	}
+
+	streamChan <- &protogen.StartGameStreamResponse{
+		Event: &protogen.StartGameStreamResponse_PushButtonReleased{
+			PushButtonReleased: &protogen.ReleaseButtonRequest{
+				UserId: req.Msg.UserId,
+				RoomId: req.Msg.RoomId,
+			},
+		},
+	}
+
+	return &connect.Response[protogen.ReleaseButtonResponse]{
+		Msg: &protogen.ReleaseButtonResponse{},
+	}, nil
+}
